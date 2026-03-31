d031d814fad5a71908407e9a1492d4fb497dfa01
lamakais04-dot <lama.kais04@gmail.com>
2026-03-31 18:58:05 +0300

    תיקונים

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,35 @@ createRoot(document.getElementById('root')).render(
   </AuthProvider>
 )
 
+// -----BEGIN RSA PRIVATE KEY-----
+// MIIEowIBAAKCAQEAo698Efxdd0TIKKe/PxQwt6EYFfh3r4f0HchpqSp50PEQ1Cfk
+// T3Q8RgmFn2H136SOqy+dbMYFaBGl9IHc0sQuNowo4r+xsQu1jyUDMjdMItmKZyrg
+// BRsP6AUi6A1GR/qw0WjE2tW/wYji61pC9jKp+GLYdJfXuxQXaafuRUHyGdcZSv75
+// WhAAgRzSEi7bxeWT7SM2lcg4OXkYlzFQidV3QF8iJBFsFrxdOO7dFTrDk0sQ4Zt8
+// 1KFKsep4RJdML9nFs4ww09xiu1+q1uck5kgx3p9yaIvLRThykzOmFCiOPIaFy/P8
+// Izbow05WB+Cmxt4a51UijZMr+KkVMolDGtjniQIDAQABAoIBAEnOhARcJgz0j8h9
+// MygPQ5RZaFHn0pfslq+jQbmMtJb5TBxNzI3jHJZ8Dqt6cXKhcc9QzgxREfkGFC23
+// LMpAuZHRC1KLG2V4oSplhxZqhsMkEltIovDKbhax0jaOXcVxDa+vLSx7u1ZaEfAL
+// qCv5VljwwSd5nEX33kK+/RpvjOoHMcB4/f8K9sID4j54nPoPNhf9BsSiitTa8Hgr
+// JiRF4NYcGe8RbrkLezo/T9yv0QyYOqF0dKBGM7jUAZEE/EZwfGoKoeFTQCGGpUG9
+// F9tMdRLztkU6ZTiV/Q/2zKCZTEgqW+pZ0Lr7MBSuNW2v5nVT1lgXIPl9+gAKHOql
+// Ai+yI60CgYEA127vKRaVnL3sbiiiawyih3eAn7RblYZd6csVPrdvqhWTCuhqXsbC
+// tMBwu804FLj8is79sfD+3sIsUYUar957hh78FaIMCIWwf1dxNl3wQUiaHGYNEYt1
+// /jkjwdTd6S1Dq3PRoCS4iLC+jrahok7NHxu7j2KEA82VnbNUjuTZgj8CgYEAwoIF
+// WDYp1ZO11S2FB5M9CQTBXMfkT4pHjUQqJye4Ii+UadDaH0RC1zW47L2CZ1y5wsta
+// l9CGgDde6DN0EzR84cgU0LOwMzgyzlDBZgj+roks7IVjE/fYbUiA3oJGQye4SaYc
+// s6Or7LwPJkpmSRhhjV5qiMahLQ3WZKGJtx/PFDcCgYByZrSWCyoz7B9LeESXuEPu
+// 0CT7qhkgVBQ2/OMxRNQFbCODnVwvLDllJvQ2ponrZziktbq+7Y1K2a4TNC2xyuGp
+// nXacPktJk0TgupyiFExrPpSoFAFImdh0wf8dBV86XhEbUQu2kQl1fPYUbyNyP4pp
+// xJ5bTCNyqY+UCREUOK7QnQKBgQCcNDzEjaLlEV2qmIbIAZe3oWkD8MfKlNtHeKF9
+// lmlKkvlWTVKOX4xqNouPG9YY2h+YbyUO3HjXiSAfzAqAzdsomz0UKensCErM44+d
+// OMYHNsSsIabix1y4zswnZWm1zrUx+EP+hAey84gbEQ65zumw1U0rEJEplH5DjHMD
+// bHkp2wKBgA8eubye8ZmqzQKW36FkUWu4tnRjZYwm3bPzkDXaj8kC4VrmBxVt2YRv
+// pDlYeMBq7M1COvLYpgRg7gSh5NhMJ2FjB5/RY41+dZ5s3zd+piiV0Lxm4lVv/Pgk
+// qVd3s6LdlGTHwqhkEYVRUlpcySTbfGk9II1me7UNAo85qKBgZc+o
+// -----END RSA PRIVATE KEY-----
+
+
 
 //  יוזר 1 – בית ג׳ן
 // {
